73cbcf15533c482ec4c1eefad4c9ab29918875c3
yumekiti <yumekiti120440@gmail.com>
2022-02-04 19:04:36 +0900

    chenge: test

--- src/jest/App.test.js
+++ src/jest/App.test.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../pages/App.js';
+import App from '../pages/App';
 
 test('Link changes the class when hovered', () => {
   const component = renderer.create(
     <App />
   );
   let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  if (tree.props.onMouseEnter) {
-    tree.props.onMouseEnter();
-  }
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  if (tree.props.onMouseLeave) {
-    tree.props.onMouseLeave();
-  }
-  // re-rendering
-  tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
